d2918295c8e7d9a833b5f4691abe7edd870eb49a
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-03-02 12:09:23 +0000

    user signup

---
+++ javascript/v2.js
@@ -0,0 +1,31 @@
+// variable type : constant (const)
+
+
+const name = " abc123";
+
+//name = "monu "   --> it is the constant keyword which is used so we cannot change .
+
+var fullName = "sonu mondal ";
+var firstName = "sonu ";
+var last_name = "mondal";
+var email = "sonu@lco.dev";
+var password = "123456";
+var confirmPassword = "123456";
+var country_from = "INDIA";
+var stateFrom = "kolkata ";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+// we took it from the user  and the best way we can use to represent things  : 
+
+console.log(`
+
+USERNAME : ${fullName }
+FIRST NAME : ${firstName}
+LAST NAME :${last_name}
+EMAIL : ${email}
+PASSWORD :${password}
+
+
+`)
