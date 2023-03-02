09fb176740b3f7ad2e4534c0b32af7254a221fe7
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-03-02 12:46:52 +0000

    if else done

---
+++ javascript/ifelse.js
@@ -0,0 +1,19 @@
+// if else ;
+
+
+// process 1 :
+var name1 = true ;
+
+if (name1){
+    console.log("sonu ");
+}else {
+    console.log("wrong name ");
+}
+
+
+
+name1=false;
+
+
+// process 2 : 
+name1 ? console.log("sonu "):console.log("wrong name ");
