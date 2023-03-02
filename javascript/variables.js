a6fbda9a9fb833d9fe799afae293c84490ea5587
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-03-02 12:00:29 +0000

    var variable

---
+++ javascript/variables.js
@@ -0,0 +1,18 @@
+// data type : var 
+
+var full_name = " sonu " ;
+
+// var full_name = " sonu " ;
+// var full_name = " sonu " ;    to do in bunch select in bunch ctrl /
+// var full_name = " sonu " ;
+
+var log_in = " done ";   // it can  store string 
+
+var log_out = 14 ;   // it can store number -> integer 
+
+var payment;   // we don't know the mode of payment 
+
+console.log(log_in);  // done 
+console.log("log");   // string is displayed .
+
+console.log(payment);  // undefined 
