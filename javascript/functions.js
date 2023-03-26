83faf38adc931666d5f407b06b7bd6456f9538c1
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-03-26 11:35:42 +0000

    done functions

---
+++ javascript/functions.js
@@ -0,0 +1,29 @@
+//functions : 
+
+//1.normal function :
+
+function sayhello(){
+
+    console.log("hello guys");
+};
+
+sayhello();  // function call 
+//-------------------------------------------------------------------------------------
+//2.function in variable :
+
+n1 = function (){
+    console.log("supratick");
+};
+
+n1(); // function call 
+//-------------------------------------------------------------------------------------
+//3.arrow function call
+
+var arrow =(p,q) =>{
+
+    console.log(p+q);
+}
+
+arrow(2,3); //function call 
+
+//-------------------------------------------------------------------------------------
