58197398e17dda7734e14001be231fd8c0ad2b01
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-10-09 07:34:22 +0000

    done

---
+++ Backend /script1.js
@@ -0,0 +1,9 @@
+//import syntax :
+
+var {a,b} = require('./script2.js');
+
+console.log(a);
+
+// var b =  require('./script2.js');
+
+console.log(b);
---
+++ Backend /script2.js
@@ -0,0 +1,16 @@
+// var a = 12 ;
+// var b = 22 ;
+
+// ek file ek e chis e export karega .
+
+// but we want to export both .
+
+// module.exports = {first : a , second : b};
+
+// module.exports = a ;
+
+
+var a = 12;
+var b = 22;
+
+module.exports = { first: a, second: b };
