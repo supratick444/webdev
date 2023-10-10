1e9100c9c0dbafcaf3248d3bbf9f8b2664840d18
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-10-10 12:30:21 +0000

    new file for npm added

---
+++ Backend /np.js
@@ -0,0 +1,20 @@
+
+// npm => node pacakge manager 
+
+// npm => files of packages which has already been coded or made before .
+
+//npm => is installed with node js  but we need to learn the usage .
+
+//npm i package_name or npm install package_name 
+
+// where do we get this package from :
+// npm js website 
+
+// example one liner joke copy and paste in the command line .
+
+//Steps to follow :
+//install the package 
+//use the package usage already given in the website .
+//output of it !!
+
+
